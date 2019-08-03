1aeb4e96adc7e423cfb98d1b18069c7f79cc22ec
Sushil Madnani <sushilmadnani@gmail.com>
2019-08-03 23:30:50 +0530

    Update call to dynamically add new data

--- WebD3BarChart/WebContent/js/main2.js
+++ WebD3BarChart/WebContent/js/main2.js
@@ -16,70 +16,95 @@ var svg = d3.select("#chart-area").append("svg")
 var g = svg.append("g")
 		.attr("transform", "translate("+ margin.left + "," + margin.top + ")");
 
-//X Label
-g.append("text")
-    .attr("class", "x axis-label")
-    .attr("x", width / 2)
-    .attr("y", height + 60)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("Month");
-
-// Y Label
-g.append("text")
-    .attr("class", "y axis-label")
-    .attr("x", - (height / 2))
-    .attr("y", -60)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("Revenue");
-
-d3.json("./data/revenues.json").then(function(data){
-	
-/*	data.forEach(function(d){
- 		d.height = +d.height;
-		console.log("name = "+d.name + " height = "+d.height);
-	});*/
-	
-	var x = d3.scaleBand()
-	.domain(data.map(function(d){
-		return d.month;
-	}))
-	.range([0, width])
-	.paddingInner(0.3)
-	.paddingOuter(0.3);
-	
-	var y = d3.scaleLinear()
-			.domain([0, d3.max(data, function(d){
-				return +d.revenue;
-			})])
-			.range([height, 0]);	
-	
-	var xAxisCall = d3.axisBottom(x);
-	g.append("g")
+var xAxisGroup = g.append("g")
 		.attr("class", "x axis")
-		.attr("transform", "translate(0, "+ height +")")
-		.call(xAxisCall);
+		.attr("transform", "translate(0, "+ height +")");
+		
+
+var yAxisGroup = g.append("g")
+		.attr("class", "y axis");
+
+
+	// X scale
+	var x = d3.scaleBand()
+		.range([0, width])
+		.paddingInner(0.3)
+		.paddingOuter(0.3);
+
+	// Y scale
+	var y = d3.scaleLinear()
+		.range([height, 0]);	
 	
+	//X Label
+	g.append("text")
+	    .attr("class", "x axis-label")
+	    .attr("x", width / 2)
+	    .attr("y", height + 60)
+	    .attr("font-size", "20px")
+	    .attr("text-anchor", "middle")
+	    .text("Month");
+	
+	// Y Label
+	g.append("text")
+	    .attr("class", "y axis-label")
+	    .attr("x", - (height / 2))
+	    .attr("y", -60)
+	    .attr("font-size", "20px")
+	    .attr("text-anchor", "middle")
+	    .attr("transform", "rotate(-90)")
+	    .text("Revenue");
+	
+	
+	d3.json("./data/revenues.json").then(function(data){
+		
+/*	data.forEach(function(d){
+ 		d.revenue = +d.revenue;
+	});*/
+		
+		update(data);
+	
+		d3.interval(function(){
+			update(data);
+		}, 1000);
+	}).catch (function(error) {
+		console.log(error);
+	});
+
+function update(data) {
+	x.domain(data.map((d) => d.month));
+	y.domain([0, d3.max(data, (d) => +d.revenue)]);
+	
+	// X Axis
+	var xAxisCall = d3.axisBottom(x);
+		xAxisGroup.call(xAxisCall);
+	
+	// Y Axis
 	var yAxisCall = d3.axisLeft(y)
 		.ticks(9)
 		.tickFormat((d) => "$" + d);
+		yAxisGroup.call(yAxisCall);
 	
-	g.append("g")
-		.attr("class", "y axis")
-		.call(yAxisCall);
-	
+	// JOIN new data with old elements.	
 	var rects = g.selectAll("rect")
-	.data(data)
-	.enter()
-	.append("rect")
-	.attr("x", (d) => x(d.month))
-	.attr("y", (d) => y(+d.revenue))
-	.attr("height", (d) => height - y(+d.revenue))
-	.attr("width", (d) => x.bandwidth())
-	.attr("fill", "black");
-		
-}).catch (function(error) {
-	console.log(error);
-});
+		.data(data);
+	
+	// Exit old elements not present in new data.
+	rects.exit().remove();
+	
+	// UPDATE old elements present in new data.
+	rects
+		.attr("x", (d) => x(d.month))
+		.attr("y", (d) => y(+d.revenue))
+		.attr("height", (d) => height - y(+d.revenue))
+		.attr("width", (d) => x.bandwidth())
+		.attr("fill", "black");
+	
+	// Enter new elements present in new data.
+	rects.enter()
+		.append("rect")
+		.attr("x", (d) => x(d.month))
+		.attr("y", (d) => y(+d.revenue))
+		.attr("height", (d) => height - y(+d.revenue))
+		.attr("width", (d) => x.bandwidth())
+		.attr("fill", "black");
+}
