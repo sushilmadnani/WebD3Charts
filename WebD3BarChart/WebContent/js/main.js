087625455248a597beae7063e708c43084adf851
Sushil Madnani <sushilmadnani@gmail.com>
2019-08-03 14:40:01 +0530

    First Real Time Bar Chart Project

---
+++ WebD3BarChart/WebContent/js/main.js
@@ -0,0 +1,89 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*/
+
+var margin = {left:150, right:10, top:10, bottom:150};
+
+var width = 600 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart-area").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom);
+
+var g = svg.append("g")
+		.attr("transform", "translate("+ margin.left + "," + margin.top + ")");
+
+//X Label
+g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 140)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("The word's tallest buildings");
+
+// Y Label
+g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", - (height / 2))
+    .attr("y", -60)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Height (m)");
+
+d3.json("./data/buildings.json").then(function(data){
+	
+/*	data.forEach(function(d){
+ 		d.height = +d.height;
+		console.log("name = "+d.name + " height = "+d.height);
+	});*/
+	
+	var x = d3.scaleBand()
+	.domain(data.map(function(d){
+		return d.name;
+	}))
+	.range([0, width])
+	.paddingInner(0.3)
+	.paddingOuter(0.3);
+	
+	var y = d3.scaleLinear()
+			.domain([0, d3.max(data, function(d){
+				return +d.height;
+			})])
+			.range([height, 0]);	
+	
+	var xAxisCall = d3.axisBottom(x);
+	g.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0, "+ height +")")
+		.call(xAxisCall)
+		.selectAll("text")
+			.attr("y", "10")
+			.attr("x", "-5")
+			.attr("text-anchor", "end")
+			.attr("transform", "rotate(-40)");
+	
+	var yAxisCall = d3.axisLeft(y)
+		.ticks(3)
+		.tickFormat((d) => d + "m");
+	
+	g.append("g")
+		.attr("class", "y axis")
+		.call(yAxisCall);
+	
+	var rects = g.selectAll("rect")
+	.data(data)
+	.enter()
+	.append("rect")
+	.attr("x", (d) => x(d.name))
+	.attr("y", (d) => y(+d.height))
+	.attr("height", (d) => height - y(+d.height))
+	.attr("width", (d) => x.bandwidth())
+	.attr("fill", "grey");
+		
+}).catch (function(error) {
+	console.log(error);
+});
